fa16ad6dfd7cef34025a31f3897f4a1d489d925e
Gurvir <GurvirSD@Gmail.com>
2026-09-12 22:44:29 -0700

    Add source directory argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Welcome to my RustSync Project!");
+    println!("RustSync");
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 2 {
+        println!("Source directory: {}", args[1]);
+    } else {
+        println!("Usage: rustsync <source_directory>");
+    }
 }
