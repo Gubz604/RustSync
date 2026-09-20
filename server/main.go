9913e731efd801c3ad59de3f304ad017f1359fd2
Gurvir <GurvirSD@Gmail.com>
2026-09-20 05:46:36 -0700

    Add Go health endpoint

--- server/main.go
+++ server/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "OK")
+}
 
 func main() {
 	fmt.Println("RustSync Backup Server")
+
+	http.HandleFunc("/health", healthHandler)
+
+	fmt.Println("Listening on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println(err)
+	}
 }
