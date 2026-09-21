e26592241a9e9341efd30ff5cf5497a069d3f1a4
Gurvir <GurvirSD@Gmail.com>
2026-09-20 18:55:32 -0700

    Add backup metadata endpoint

--- server/main.go
+++ server/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type Backup struct {
+	Path string `json:"path"`
+	Size uint64 `json:"size"`
+	Hash string `json:"hash"`
+}
+
 func healthHandler(w http.ResponseWriter, r *http.Request) {
-	if !(r.Method == http.MethodGet) {
+	if r.Method != http.MethodGet {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	} else {
@@ -15,10 +22,33 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func backupHandler(w http.ResponseWriter, r *http.Request) {
+	var backup Backup
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	} 
+	
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&backup); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println(backup)
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "OK")
+}
+
 func main() {
 	fmt.Println("RustSync Backup Server")
 
 	http.HandleFunc("/health", healthHandler)
+
+	http.HandleFunc("/backup", backupHandler)
 
 	fmt.Println("Listening on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
